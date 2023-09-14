7fdc1e4b051f0fee264fab2b95fba469405d57cd
rachit-noob <rachitkumarsinghismyname@gmail.com>
2023-09-14 23:42:34 +0530

    App changes

--- stream_app.py
+++ stream_app.py
@@ -21,17 +21,18 @@ product_importance = st.number_input('Product Importance', min_value=0)
 gender = st.selectbox('Gender', ['Male', 'Female'])
 discount_offered = st.number_input('Discount Offered', step=0.01)
 weight_in_gms = st.number_input('Weight in grams', step=0.01)
-warehouse_block = st.selectbox('Warehouse Block', ['A', 'B', 'C', 'D'])
-mode_of_shipment = st.selectbox('Mode of Shipment', ['Flight', 'Road'])
+warehouse_block = st.selectbox('Warehouse Block', ['A', 'B', 'C', 'D', 'F'])
+mode_of_shipment = st.selectbox('Mode of Shipment', ['Flight', 'Road', 'Ship'])
 
-# Map gender, warehouse_block, and mode_of_shipment to numeric values
-gender_mapping = {'Male': 1, 'Female': 0}
-warehouse_mapping = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
-shipment_mapping = {'Flight': 1, 'Road': 0}
+# Map gender, warehouse_block, and mode_of_shipment to one-hot encoded values
+gender_mapping = {'Male': [1, 0], 'Female': [0, 1]}
+warehouse_mapping = {'A': [1, 0, 0, 0, 0], 'B': [0, 1, 0, 0, 0], 'C': [0, 0, 1, 0, 0], 'D': [0, 0, 0, 1, 0], 'F': [0, 0, 0, 0, 1]}
+shipment_mapping = {'Flight': [1, 0, 0], 'Road': [0, 1, 0], 'Ship': [0, 0, 1]}
 
-gender_numeric = gender_mapping.get(gender, 0)
-warehouse_numeric = warehouse_mapping.get(warehouse_block, 0)
-shipment_numeric = shipment_mapping.get(mode_of_shipment, 0)
+# Get the one-hot encoded values based on user selection
+gender_encoded = gender_mapping.get(gender, [0, 0])
+warehouse_encoded = warehouse_mapping.get(warehouse_block, [0, 0, 0, 0])
+shipment_encoded = shipment_mapping.get(mode_of_shipment, [0, 0, 0])
 
 # Predict when the user clicks the "Predict" button
 if st.button('Predict'):
@@ -41,15 +42,17 @@ if st.button('Predict'):
         'Cost_of_the_Product': [cost_of_the_product],
         'Prior_purchases': [prior_purchases],
         'Product_importance': [product_importance],
-        'Gender': [gender_numeric],
+        'Gender_Male': [gender_encoded[0]],
+        'Gender_Female': [gender_encoded[1]],
         'Discount_offered': [discount_offered],
         'Weight_in_gms': [weight_in_gms],
-        'Warehouse_block_A': [1 if warehouse_numeric == 0 else 0],
-        'Warehouse_block_B': [1 if warehouse_numeric == 1 else 0],
-        'Warehouse_block_C': [1 if warehouse_numeric == 2 else 0],
-        'Warehouse_block_D': [1 if warehouse_numeric == 3 else 0],
-        'Mode_of_Shipment_Flight': [shipment_numeric],
-        'Mode_of_Shipment_Road': [1 if shipment_numeric == 0 else 0]
+        'Warehouse_block_A': [warehouse_encoded[0]],
+        'Warehouse_block_B': [warehouse_encoded[1]],
+        'Warehouse_block_C': [warehouse_encoded[2]],
+        'Warehouse_block_D': [warehouse_encoded[3]],
+        'Mode_of_Shipment_Flight': [shipment_encoded[0]],
+        'Mode_of_Shipment_Road': [shipment_encoded[1]],
+        'Mode_of_Shipment_Ship': [shipment_encoded[2]]
     })
 
     try:
